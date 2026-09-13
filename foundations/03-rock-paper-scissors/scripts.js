b76f80325677c8fb1febda147992612b716abc29
Evan Nartea <evannartea@Mac.lan>
2026-09-13 11:41:58 +0100

    complete rock paper scissors game

--- foundations/03-rock-paper-scissors/scripts.js
+++ foundations/03-rock-paper-scissors/scripts.js
@@ -1,7 +1,4 @@
 const choices = ["rock", "paper", "scissors"];
-// Declare score variables
-let humanScore = 0;
-let computerScore = 0;
 
 // Function to get computer's choice
 function getComputerChoice() {
@@ -14,31 +11,69 @@ function getComputerChoice() {
 // Function to get human's choice
 function getHumanChoice() {
     // Prompt user input for choice
-    return prompt("Rock, paper, or scissors: ");
+    return prompt("Choose Rock, Paper, or Scissors: ");
 }
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toLowerCase() === computerChoice) {
-        console.log("Draw!");
+// Function to capitalise strings
+function capitaliseString(str) {
+    const firstLetter = str[0].toUpperCase()
+    let otherLetters = ""
+
+    for (let i = 1; i < str.length; i++) {
+        otherLetters += str[i].toLowerCase()
     }
-    else if (
-        (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
-        (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
-        (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper")
-    ) {
-        console.log(`You win! ${humanChoice.toLowerCase()} beats ${computerChoice}!`);
-        humanScore++;
+
+    return `${firstLetter}${otherLetters}`
+}
+
+function playGame() {
+    // Declare score variables
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        // If round is draw
+        if (humanChoice.toLowerCase() === computerChoice) {
+            console.log("Draw!");
+        }
+        // If human beats computer
+        else if (
+            (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
+            (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
+            (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper")
+        ) {
+            // Log win message
+            console.log(`You win this round! ${capitaliseString(humanChoice)} beats ${capitaliseString(computerChoice)}!`);
+            // Increment human score
+            humanScore++;
+        }
+        // If computer beats human
+        else {
+            // Log lose message
+            console.log(`You lose this round! ${capitaliseString(computerChoice)} beats ${capitaliseString(humanChoice)}!`);
+            // Increment computer score
+            computerScore++;
+        }
+    }
+
+    for (i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+
+        console.log(`Player: ${humanScore}\nComputer: ${computerScore}`);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("Player wins the game!")
+    }
+    else if (computerScore > humanScore) {
+        console.log("Computer wins the game!")
     }
     else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice.toLowerCase()}!`);
-        computerScore++;
+        console.log("It's a draw!")
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
-
-console.log(humanScore);
-console.log(computerScore);
+playGame()
