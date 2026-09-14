8a26567bb227b4003d48c766086683c8df1d4e54
Evan Nartea <evannartea@Mac.lan>
2026-09-14 17:00:52 +0100

    add ;

--- foundations/assignments/loops-and-arrays/array-methods-exercises/01-translate-border-left-width-to-borderLeftWidth.js
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/01-translate-border-left-width-to-borderLeftWidth.js
@@ -1,18 +1,18 @@
 function camelise(str) {
     // Split into separate words
-    const strArray = str.split("-")
+    const strArray = str.split("-");
 
     // Capitalise elements excluding first
     const capitalised = strArray.slice(1).map(function(word) {
-        return word[0].toUpperCase() + word.slice(1)
+        return word[0].toUpperCase() + word.slice(1);
     })
 
     // Combine first element with capitalised elements
-    const camelCase = strArray[0] + capitalised.join("")
+    const camelCase = strArray[0] + capitalised.join("");
 
-    return camelCase
+    return camelCase;
 }
 
-console.log(camelise("background-colour"))
-console.log(camelise("list-style-image"))
-console.log(camelise("-webkit-transition"))
+console.log(camelise("background-colour"));
+console.log(camelise("list-style-image"));
+console.log(camelise("-webkit-transition"));
---
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/02-filter-range.js
@@ -0,0 +1,7 @@
+function filterRange(arr, a, b) {
+
+}
+
+let arr = [5, 3, 8, 1];
+
+let filteredArr = filterRange(arr, 1, 4);
