d2637725d5254bf4fe74dbe232914a630d971945
Evan Nartea <evannartea@Mac.lan>
2026-09-14 16:22:10 +0100

    finish camel case function

---
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/01-translate-border-left-width-to-borderLeftWidth.js
@@ -0,0 +1,18 @@
+function camelise(str) {
+    // Split into separate words
+    const strArray = str.split("-")
+
+    // Capitalise elements excluding first
+    const capitalised = strArray.slice(1).map(function(word) {
+        return word[0]?.toUpperCase() + word.slice(1)
+    })
+
+    // Combine first element with capitalised elements
+    const camelCase = strArray[0] + capitalised.join("")
+
+    return camelCase
+}
+
+console.log(camelise("background-colour"))
+console.log(camelise("list-style-image"))
+console.log(camelise("-webkit-transition"))
