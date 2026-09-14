668bc1fc35b1578bac42fc158dcb921126508727
Evan Nartea <evannartea@Mac.lan>
2026-09-14 17:30:25 +0100

    finish filterRange

--- foundations/assignments/loops-and-arrays/array-methods-exercises/02-filter-range.js
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/02-filter-range.js
@@ -1,7 +1,20 @@
 function filterRange(arr, a, b) {
+    // Create empty array
+    let filteredArr = [];
 
+    // Loop through arr
+    for (let i = 0; i < arr.length; i++) {
+        // Return if element <= a and >= b
+        if (a <= arr[i] && arr[i] <= b) {
+            filteredArr.push(arr[i]);
+        }
+    }
+
+    return filteredArr;
 }
 
 let arr = [5, 3, 8, 1];
 
-let filteredArr = filterRange(arr, 1, 4);
+let filtered = filterRange(arr, 1, 4);
+
+console.log(filtered);
