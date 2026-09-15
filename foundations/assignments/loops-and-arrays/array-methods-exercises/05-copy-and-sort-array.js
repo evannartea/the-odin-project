db9b0cb0e07ebb4deb56cdb1a844ebcf668de5da
Evan Nartea <evannartea@Mac.lan>
2026-09-15 12:55:42 +0100

    finish copy and sort array

---
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/05-copy-and-sort-array.js
@@ -0,0 +1,18 @@
+function copySorted(arr) {
+    // dawg i don't know
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+
+let sorted = copySorted(arr);
+
+console.log(sorted);
+console.log(arr);
+
+/*
+solution:
+
+function copySorted(arr) {
+  return arr.slice().sort();
+}
+*/
