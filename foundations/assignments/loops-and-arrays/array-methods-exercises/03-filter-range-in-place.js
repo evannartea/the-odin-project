37d094df11f8fd73e5cd7cf445e9941fae133959
Evan Nartea <evannartea@Mac.lan>
2026-09-15 11:20:26 +0100

    finish filter range in place

---
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/03-filter-range-in-place.js
@@ -0,0 +1,12 @@
+function filterRangeInPlace(arr, a, b) {
+    return arr.filter(function(element) {
+        return (a <= element && element <= b)
+    })
+}
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRangeInPlace(arr, 1, 4);
+
+console.log(filtered);
+
