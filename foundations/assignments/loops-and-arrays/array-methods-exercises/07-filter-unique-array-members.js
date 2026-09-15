5f10ee9cbced736fdcc8ad299f54b1c123b5c447
Evan Nartea <evannartea@Mac.lan>
2026-09-15 16:57:27 +0100

    rename filename

---
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/07-filter-unique-array-members.js
@@ -0,0 +1,12 @@
+function unique(arr) {
+    return arr.filter(function(){
+        
+    });
+}
+
+let strings = [
+    "Hare", "Krishna", "Hare", "Krishna",
+    "Krishna", "Krishna", "Hare", "Hare", ":-O"
+];
+
+console.log(unique(strings));
