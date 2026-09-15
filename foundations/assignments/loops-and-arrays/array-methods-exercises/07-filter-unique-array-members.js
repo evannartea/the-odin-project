2d413bd6f1c02cf49ffdb9d27eec4908f08ddc26
Evan Nartea <evannartea@Mac.lan>
2026-09-15 17:30:45 +0100

    finish filter unique array members

--- foundations/assignments/loops-and-arrays/array-methods-exercises/07-filter-unique-array-members.js
+++ foundations/assignments/loops-and-arrays/array-methods-exercises/07-filter-unique-array-members.js
@@ -1,6 +1,6 @@
 function unique(arr) {
-    return arr.filter(function(){
-        
+    return arr.filter(function(str, index, array) {
+        return array.indexOf(str) === index;
     });
 }
 
@@ -10,3 +10,24 @@ let strings = [
 ];
 
 console.log(unique(strings));
+
+
+/*
+solution:
+
+function unique(arr) {
+    // Create empty array
+    let uniqueArr = [];
+
+    // Loop through given array
+    for (let i = 0; i < arr.length; i++) {
+
+        // If empty array does not already include element of given array at index i, add it
+        if (!uniqueArr.includes(arr[i])) {
+            uniqueArr.push(arr[i]);
+        }
+    }
+
+    return uniqueArr;
+}
+ */
