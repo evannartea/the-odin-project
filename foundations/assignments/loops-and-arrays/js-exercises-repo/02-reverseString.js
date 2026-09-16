c3bfb6a7c4049369157b42d7b35a6f875a348abe
Evan Nartea <evannartea@Mac.lan>
2026-09-16 11:28:01 +0100

    finish reverseString

---
+++ foundations/assignments/loops-and-arrays/js-exercises-repo/02-reverseString.js
@@ -0,0 +1,36 @@
+const reverseString = function(str) {
+    let reversed = "";
+
+    for (let i = 0; i < str.length; i++) {
+        reversed += str[str.length - (i + 1)]
+    }
+    return reversed;
+}
+
+console.log(reverseString("hello there"));
+
+/*
+h e l l o _ t h e r e
+0 1 2 3 4 5 6 7 8 9 10
+
+str[0] -> str[str.length - 1]
+str[1] -> str[str.length - 2]
+            .
+            .
+            .
+str[i] -> str[str.length - (i + 1)]
+*/
+
+/*
+solution:
+
+const reverseString = function (string) {
+    let reversedString = "";
+
+    for (let i = string.length -1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString;
+};
+
+*/
