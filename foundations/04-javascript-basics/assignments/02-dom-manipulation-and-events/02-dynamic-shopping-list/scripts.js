5836e2bfe8e239238f47cae99920c6aa3f22a3fb
Evan Nartea <evannartea@Mac.lan>
2026-09-21 11:58:56 +0100

    finish dynamic-shopping-list

--- foundations/04-javascript-basics/assignments/02-dom-manipulation-and-events/02-dynamic-shopping-list/scripts.js
+++ foundations/04-javascript-basics/assignments/02-dom-manipulation-and-events/02-dynamic-shopping-list/scripts.js
@@ -1,1 +1,27 @@
-console.log("peepeepoopoo")
+const unorderedList = document.querySelector("ul");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const inputItem = input.value;
+    console.log(inputItem);
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const deleteBtn = document.createElement("button");
+
+    listItem.appendChild(listText);
+    listText.textContent = inputItem;
+    listItem.appendChild(deleteBtn);
+    deleteBtn.textContent = "Delete";
+    unorderedList.appendChild(listItem);
+
+    deleteBtn.addEventListener("click", function (event) {
+        unorderedList.removeChild(listItem);
+    });
+
+    input.focus();
+});
