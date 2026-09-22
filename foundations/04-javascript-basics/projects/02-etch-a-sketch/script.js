680af108e902e958d060bd7548a68b336680b993
Evan Nartea <evannartea@Mac.lan>
2026-09-22 17:39:11 +0100

    add generateGrid function

--- foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
+++ foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
@@ -1,4 +1,18 @@
 function generateGrid(n) {
-    const container = document.querySelector("#grid-container");
-    const div = document.createElement("div");
+    const screenContainer = document.querySelector("#screen-container");
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            const gridTile = document.createElement("div");
+            gridTile.style.cssText = `
+                width: 10px;
+                height: 10px;
+
+                border: 1px solid black;
+            `;
+            screenContainer.appendChild(gridTile);
+        }
+    }
 };
+
+generateGrid(2);
