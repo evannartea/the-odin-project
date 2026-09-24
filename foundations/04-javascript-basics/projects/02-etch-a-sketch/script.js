5ac5fde3ef7d49b8bed541d670c7c82178e54c09
Evan Nartea <evannartea@Mac.lan>
2026-09-24 11:34:57 +0100

    add dynamic wxh calc to gridTiles

--- foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
+++ foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
@@ -1,14 +1,18 @@
+// Function to generate nxn grid
 function generateGrid(n) {
     const screenContainer = document.querySelector("#screen-container");
 
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             const gridTile = document.createElement("div");
-            gridTile.style.cssText = `
-                width: 10px;
-                height: 10px;
+            gridTile.classList.add("grid-tile");
 
-                border: 1px solid black;
+            gridTile.style.cssText = `
+                width: ${(100/n)}%;
+                height: ${(100/n)}%;
+
+                box-sizing: border-box;
+                border: 1px solid grey;
             `;
             screenContainer.appendChild(gridTile);
         }
@@ -16,3 +20,12 @@ function generateGrid(n) {
 };
 
 generateGrid(2);
+
+/*
+1x1 grid -> 100% wxh
+2x2 grid -> 50% wxh
+        .
+        .
+        .
+nxn grid -> 100/n% wxh
+*/
