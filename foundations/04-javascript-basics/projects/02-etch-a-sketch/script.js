92a6c4725a26759087ce7ec0b640007f0d7cb75b
Evan Nartea <evannartea@Mac.lan>
2026-09-24 13:06:33 +0100

    add changeColourOnHover function

--- foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
+++ foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
@@ -1,31 +1,38 @@
-// Function to generate nxn grid
-function generateGrid(n) {
-    const screenContainer = document.querySelector("#screen-container");
+const screenContainer = document.querySelector("#screen-container");
 
+// Function to generate nxn grid of divs
+/*
+1x1 grid -> 100% w*h
+2x2 grid -> 50% w*h
+        .
+        .
+        .
+nxn grid -> 100/n% w*h
+*/
+function generateGrid(n) {
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n; j++) {
             const gridTile = document.createElement("div");
             gridTile.classList.add("grid-tile");
-
             gridTile.style.cssText = `
-                width: ${(100/n)}%;
-                height: ${(100/n)}%;
+                width: ${100/n}%;
+                height: ${100/n}%;
 
                 box-sizing: border-box;
-                border: 1px solid grey;
+                border: 0.5px solid grey;
             `;
+
+            changeColourOnHover(gridTile);
             screenContainer.appendChild(gridTile);
         }
     }
 };
 
-generateGrid(2);
+// Function to change colour of <div>
+function changeColourOnHover(element) {
+    element.addEventListener("mouseover", function(event) {
+        event.target.style.backgroundColor = "black";
+    });
+};
 
-/*
-1x1 grid -> 100% wxh
-2x2 grid -> 50% wxh
-        .
-        .
-        .
-nxn grid -> 100/n% wxh
-*/
+generateGrid(16);
