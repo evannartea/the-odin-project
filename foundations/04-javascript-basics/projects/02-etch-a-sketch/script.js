5adf6b6e9d1f34ffb4ef59c570fa8d3f119e2253
Evan Nartea <evannartea@Mac.lan>
2026-09-25 13:26:04 +0100

    add grid size functions

--- foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
+++ foundations/04-javascript-basics/projects/02-etch-a-sketch/script.js
@@ -3,9 +3,6 @@ const rangeSlider = document.querySelector("#range-slider");
 const rangeValue = document.querySelector("#range-value");
 const MIN_GRID_SIZE = 16;
 
-generateGrid(MIN_GRID_SIZE);
-rangeValue.textContent = `${MIN_GRID_SIZE}x${MIN_GRID_SIZE} GRID`;
-
 // Function to generate nxn grid of divs
 /*
 1x1 grid -> 100% w*h
@@ -43,10 +40,22 @@ function changeColourOnHover(element) {
     });
 };
 
-// Change grid size according to slider
-rangeSlider.addEventListener("input", function() {
-    generateGrid(rangeSlider.value)
+// Function to display current grid size
+function showCurrentGridSize(size) {
+    return `${size}x${size} GRID`;
+};
 
-    // Change range value according to slider
-    rangeValue.textContent = `${rangeSlider.value}x${rangeSlider.value} GRID`;
-});
+// Function to update grid size according to slider
+function updateGridSize() {
+    rangeSlider.addEventListener("input", function () {
+        generateGrid(rangeSlider.value)
+
+        rangeValue.textContent = showCurrentGridSize(rangeSlider.value);
+    });
+};
+
+// Default grid
+generateGrid(MIN_GRID_SIZE);
+rangeValue.textContent = showCurrentGridSize(MIN_GRID_SIZE);
+
+updateGridSize();
